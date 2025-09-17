e441eec6590c66c8700cd98e14f5cf05db573161
chinmay <sahuchinmay27@gmail.com>
2025-09-17 10:59:09 +0530

    first commit

---
+++ terraform-manifests/c1-versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">=1.13.1"
+  required_providers {
+    google = {
+        source= "hashicorp/google"
+        version = "~> 7.2.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "cks-gcp"
+  region = ""
+}
